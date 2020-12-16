4a951fddbaeb7f865699b45c51d80cde000fb7e6
pjthepooh <huangpj1110@gmail.com>
2020-12-15 22:33:25 -0800

    add demo files

---
+++ src/main/java/com/kafka/demo/ProducerDemo.java
@@ -0,0 +1,38 @@
+package com.kafka.demo;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+
+        final Logger logger = LoggerFactory.getLogger(ProducerDemo.class);
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // create producer record
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello world");
+
+        // send data -- async
+        producer.send(record);
+
+        // flush data
+        producer.flush();
+        // flush and close producer
+        producer.close();
+    }
+}
---
+++ src/main/java/com/kafka/demo/ProducerDemoKeys.java
@@ -0,0 +1,61 @@
+package com.kafka.demo;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class ProducerDemoKeys {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        final Logger logger = LoggerFactory.getLogger(ProducerDemoKeys.class);
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for (int i=0; i<10; i++) {
+            // create producer record
+            String topic = "first_topic";
+            String key = "id_" + i;
+            String value = "hello world " + i;
+
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, key, value);
+
+            logger.info("Key: " + key);
+
+            // send data -- async
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    // executes every time a record is successfully sent or an exception is thrown
+                    if (e == null) {
+                        logger.info("Received new metadata. \n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp()
+                        );
+                    } else {
+                        logger.error("Error while producing", e);
+                    }
+                }
+            }).get();  // block the .send() to make it sync, don't do it in prod
+        }
+
+        // flush data
+        producer.flush();
+        // flush and close producer
+        producer.close();
+    }
+}
